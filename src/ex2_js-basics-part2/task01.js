906f2593767abf4f270172855c934c572967587b
epam <epam@local.rsu.edu.ru>
2018-10-05 20:18:24 +0300

    first-task

---
+++ src/ex2_js-basics-part2/task01.js
@@ -0,0 +1,19 @@
+function varType (a) {
+    typeof a;
+    if (typeof a == "number") {
+        console.log('число');
+    } else if (typeof a == "string") {
+        console.log('строка');
+    } else {
+        console.log('undefined');
+    }
+}
+
+varType('12');
+varType(74);
+varType(true);
+varType(null);
+varType(undefined);
+varType([1,2,3]);
+varType('Automobile');
+varType(234);
