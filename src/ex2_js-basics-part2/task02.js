b9691ae4084807d07d81a24ed89edac0b9dc21e2
ivkarmash <ivan-karmashov@yandex.ru>
2018-10-05 21:15:15 +0300

    second-task

--- src/ex2_js-basics-part2/task02.js
+++ src/ex2_js-basics-part2/task02.js
@@ -1,1 +1,9 @@
-console.log(12)
+function arrayLength (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+    console.log('Длина массива ' + arr.length);
+}
+
+arrayLength ([1,2]);
+arrayLength ([1,2,34]);
